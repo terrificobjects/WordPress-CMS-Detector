0f2d2798c3647c50027413e046e26ef3b6bb5abe
Terrific Objects <nick@terrificobjects.com>
2023-09-14 01:53:02 -0600

    added rate limiter by IP and hourly, as well as alert

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ let fetch;
 import('node-fetch').then(module => {
   fetch = module.default;
 });
-
 const cors = require('cors');
 const { parse } = require('node-html-parser');
 
 const app = express();
+const ipCounter = {};
+const maxRequests = 20;
+const timeLimit = 3600000;
 
 // Enable CORS for all routes
 app.use(cors());
@@ -17,7 +19,40 @@ app.use(express.json());
 
 // POST /identify endpoint
 app.post('/identify', async (req, res) => {
-    const url = req.body.url;
+    const ip = req.ip;
+    const currentTime = Date.now();
+
+    // Check if this IP has made a request before
+    if (ipCounter[ip]) {
+        if (ipCounter[ip].count >= maxRequests) {
+            const timeSinceFirstRequest = currentTime - ipCounter[ip].firstRequestTime;
+
+            if (timeSinceFirstRequest <= timeLimit) {
+                return res.status(429).json({ error: 'Rate limit exceeded', message: 'You must wait 1 hour after making 20 requests to make more.' });
+            }
+
+            // Reset counter and time if more than an hour has passed
+            ipCounter[ip].count = 0;
+            ipCounter[ip].firstRequestTime = currentTime;
+        }
+    } else {
+        // Initialize counter and time for this IP
+        ipCounter[ip] = { count: 0, firstRequestTime: currentTime };
+    }
+
+    // Increment the request count for this IP
+    ipCounter[ip].count += 1;
+
+    let url = req.body.url;
+
+    // Validate and correct the URL if necessary
+    if (!url) {
+        return res.status(400).json({ error: 'URL is required' });
+    }
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+        url = 'http://' + url;
+    }
+
     let isWordPress = false;
     let version = "Unknown";
     let theme = "Unknown";
@@ -27,11 +62,9 @@ app.post('/identify', async (req, res) => {
         const text = await response.text();
         const root = parse(text);
 
-        // Check if WordPress
         if (text.includes("wp-")) {
             isWordPress = true;
 
-            // Find version
             const generatorMeta = root.querySelector("meta[name='generator']");
             if (generatorMeta && generatorMeta.getAttribute("content")) {
                 const content = generatorMeta.getAttribute("content");
@@ -41,7 +74,6 @@ app.post('/identify', async (req, res) => {
                 }
             }
 
-            // Find theme
             const themeMatch = text.match(/\/themes\/([a-zA-Z0-9_-]+)/);
             if (themeMatch) {
                 theme = themeMatch[1];
@@ -49,6 +81,7 @@ app.post('/identify', async (req, res) => {
         }
     } catch (error) {
         console.error(error);
+        return res.status(500).json({ error: 'Something went wrong' });
     }
 
     res.json({ isWordPress, version, theme });
